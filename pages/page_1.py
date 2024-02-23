d3b5a5118b52754f9111d245b014941541fdee67
Liviru <132646852+s4021245@users.noreply.github.com>
2024-02-23 12:12:19 +1100

    First commit

---
+++ pages/page_1.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.page_link("streamlit_app.py", label="Home", icon="🏠")
+st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣")
---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+if __name__ == "__main__":
+    st.page_link("streamlit_app.py", label="Home", icon="🏠")
+    st.page_link("pages/page_1.py", label="Page 1", icon="1️⃣")
+    st.page_link("pages/page_2.py", label="Page 2", icon="2️⃣")
